5b050e18db8441176a2fbe25be955f8b03448464
huutuan238 <huutuan238@gmail.com>
2026-09-23 10:15:04 +0900

    fix: update fort

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import '@fontsource/be-vietnam-pro/400.css'; // Regular
+import '@fontsource/be-vietnam-pro/500.css'; // Medium
+import '@fontsource/be-vietnam-pro/700.css'; // Bold
 
 createApp(App).use(router).mount('#app')
